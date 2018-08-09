723238ff0cad2b38db26ee824f71023bd6619abc
guolei <guolei01@yxqiche.com>
2018-08-09 10:50:57 +0800

    xx

--- src/main/scala/com/adt/demo/test/BaseConvertion.scala
+++ src/main/scala/com/adt/demo/test/BaseConvertion.scala
@@ -1,6 +1,20 @@
 package com.adt.demo.test
 
 object BaseConvertion {
+  def decimal2Binary(decimal: Decimal) = {
+    decimal.number.toInt
+
+  }
+
+  def toBinary(org: BigInt, acc: String) = {
+    if (org < 2) {
+      org.toString + acc
+    }
+    else {
+      org.mod(2).toString() + acc
+    }
+  }
+
   def binary2Decimal(bStr:String) = {
     Binary(bStr).number.reverse.zipWithIndex.map{case (c,i) => c.toString.toInt * Math.pow(2,i)}
   }
--- src/test/scala/flat/Binary2DecimalSpec.scala
+++ src/test/scala/flat/Binary2DecimalSpec.scala
@@ -1,11 +1,21 @@
 package flat
 
-import com.adt.demo.test.{BaseConvertion, Binary}
+import com.adt.demo.test.{BaseConvertion, Binary, Decimal}
 import org.scalatest.FlatSpec
 
 class Binary2DecimalSpec extends FlatSpec{
   "base convert utilities " should  "convert 11101010  to decimal 234" in {
       val cc = BaseConvertion.binary2Decimal("11101010").sum
       assert(cc.toInt == 234)
+  }
+
+  it should  "convert 11101010 to int is 234" in {
+    val cc = BaseConvertion.binary2Decimal("11101010").sum
+    assert(cc.toInt == 234)
+  }
+
+  it should  "convert 234 to binary string is '11101010'" in {
+    val dd = BaseConvertion.decimal2Binary(Decimal("234"))
+    assert(dd.number == "11101010")
   }
 }
