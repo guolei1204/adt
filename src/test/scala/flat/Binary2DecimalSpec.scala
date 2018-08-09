6f2191451ff6d78fa4daa32e437cd9c67d5491bb
guolei <guolei01@yxqiche.com>
2018-08-09 10:41:52 +0800

    test pass

--- src/test/scala/flat/Binary2DecimalSpec.scala
+++ src/test/scala/flat/Binary2DecimalSpec.scala
@@ -6,6 +6,6 @@ import org.scalatest.FlatSpec
 class Binary2DecimalSpec extends FlatSpec{
   "base convert utilities " should  "convert 11101010  to decimal 234" in {
       val cc = BaseConvertion.binary2Decimal("11101010").sum
-      assert(cc.toInt.toString == 234)
+      assert(cc.toInt == 234)
   }
 }
