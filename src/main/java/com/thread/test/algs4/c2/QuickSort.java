07c2a55a0081f707c9aefe93e03db892dc843d13
guolei <guolei_1204@foxmail.com>
2019-04-14 11:47:19 +0800

    quick sort

--- src/main/java/com/thread/test/algs4/c2/QuickSort.java
+++ src/main/java/com/thread/test/algs4/c2/QuickSort.java
@@ -11,27 +11,30 @@ public class QuickSort {
         sort(array, 0, array.length - 1);
     }
 
+
+    //局部有序算法最优解 理解双指针
     private static void sort(Comparable[] array, int l, int r) {
         if (l >= r) return;
         Comparable pivot = array[l];
         int left = l + 1;
         int right = r;
-        while (left <= right) {
 
+        //双指针,
+        while (left <= right) {
+            // 如果左边的元素小于右边则移动指针
             while (left <= right && less(array[left], pivot)) {
                 left++;
             }
-
+            // 如果右边的元素大于基准 这右边的指针向左移动
             while (left <= right && array[right].compareTo(pivot) >= 0) {
                 right--;
             }
-
+            //如果左边的大于右边的则退出
             if (left > right) break;
-            //swap array[left] array[right] while left <= right
+            //swap 较远指针元素 array[left] array[right] while left <= right
             exchange(array, left, right);
         }
         // swap the smaller with pivot
-
         exchange(array, right, l);
 
         sort(array, l, right - 1);
